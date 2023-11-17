bf3f882116ae66f8b8d187460772c9be9b105d84
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-11-17 18:32:26 +0100

    redirect to index route if error in the post index route

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
-import requests, os, uuid, json
-from flask import Flask, redirect, url_for, request, render_template, session
+from flask import Flask, redirect, url_for, request, render_template
 from deep_translator import GoogleTranslator
 import json
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    languages_data = json.load(open('./data/languages.json'))
-    return render_template('index.html', languages=languages_data)
+    languages_data = json.load(open("./data/languages.json"))
+    return render_template("index.html", languages=languages_data)
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def index_post():
+    try:
+        original_text = request.form["text"]
+        target_language = request.form["target_language"]
+        source_language = request.form["source_language"]
 
-    original_text = request.form['text']
-    target_language = request.form['target_language']
-    source_language = request.form['source_language']
+        if not original_text or not target_language:
+            return redirect(url_for("index"))
 
-    print(target_language)
+        translation = GoogleTranslator(
+            source=source_language, target=target_language
+        ).translate(text=original_text)
 
-    body = [{ 'text': original_text }]
-    original_text = 'bonjour, je suis un humain'
+        print(translation)
 
-    translation = GoogleTranslator(source='auto', target='en').translate(text=original_text)
+        return render_template(
+            "results.html", translated_text=translation, original_text=original_text
+        )
 
-    print(translation)
-   
-    return render_template(
-        'results.html',
-        translated_text=translation,
-        original_text=original_text,
-        target_language=target_language
-    )
+    except:
+        return redirect(url_for("index"))
