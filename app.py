bb4ee44f13199df465d1719d3934fb780397abbd
UltraViolet33 <93035200+UltraViolet33@users.noreply.github.com>
2023-11-17 15:43:24 +0100

    change azure translator to deep_translator

--- app.py
+++ app.py
@@ -1,57 +1,36 @@
 import requests, os, uuid, json
-from dotenv import load_dotenv
-load_dotenv()
 from flask import Flask, redirect, url_for, request, render_template, session
+from deep_translator import GoogleTranslator
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    languages_data = json.load(open('./data/languages.json'))
+    return render_template('index.html', languages=languages_data)
 
 
 @app.route('/', methods=['POST'])
 def index_post():
-    # Read the values from the form
+
     original_text = request.form['text']
-    target_language = request.form['language']
+    target_language = request.form['target_language']
+    source_language = request.form['source_language']
 
-    # Load the values from .env
-    key = os.environ['KEY']
-    endpoint = os.environ['ENDPOINT']
-    location = os.environ['LOCATION']
+    print(target_language)
 
-    # Indicate that we want to translate and the API version (3.0) and the target language
-    path = '/translate?api-version=3.0'
-    # Add the target language parameter
-    target_language_parameter = '&to=' + target_language
-    # Create the full URL
-    constructed_url = endpoint + path + target_language_parameter
-
-    # Set up the header information, which includes our subscription key
-    headers = {
-        'Ocp-Apim-Subscription-Key': key,
-        'Ocp-Apim-Subscription-Region': location,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
-
-    # Create the body of the request with the text to be translated
     body = [{ 'text': original_text }]
+    original_text = 'bonjour, je suis un humain'
 
-    # Make the call using post
-    translator_request = requests.post(constructed_url, headers=headers, json=body)
-    # Retrieve the JSON response
-    translator_response = translator_request.json()
-    # Retrieve the translation
-    translated_text = translator_response[0]['translations'][0]['text']
+    translation = GoogleTranslator(source='auto', target='en').translate(text=original_text)
 
-    # Call render template, passing the translated text,
-    # original text, and target language to the template
+    print(translation)
+   
     return render_template(
         'results.html',
-        translated_text=translated_text,
+        translated_text=translation,
         original_text=original_text,
         target_language=target_language
     )
